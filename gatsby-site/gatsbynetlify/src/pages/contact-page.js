3a0375887bf93912b27fb73b5f66c1afa7f3a746
Deliman206 <smiller206@me.com>
2018-08-28 15:33:12 -0700

    working on deployment for netlify

---
+++ gatsby-site/gatsbynetlify/src/pages/contact-page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ContactPage = () => (
+  <div className='page'>
+    <h1>Hello from Contacts!</h1>
+    <p>Here you can get in contact with me.</p>
+  </div>
+)
+
+export default ContactPage
---
+++ gatsby-site/gatsbynetlify/src/pages/projects-page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ProjectsPage = () => (
+  <div className='page'>
+    <h1>Hello from Projects</h1>
+    <p>Here you can see my work.</p>
+  </div>
+)
+
+export default ProjectsPage
